b2ffa5b8f34f722893b6eb7684c2143392ec30fe
Alexander <alexander.pictures@yandex.ru>
2017-05-29 21:30:43 +0300

    added banners

--- tools/context_processor.py
+++ tools/context_processor.py
@@ -3,5 +3,6 @@ from django.conf import settings
 def toolset_proc(request):
     return {
             'GOOGLE_MAPS_KEY': settings.GOOGLE_MAPS_KEY,
-            'MAIN_SITE_REDIRECT': 'https://zeroelement.com'
+            'MAIN_SITE_REDIRECT': 'https://zeroelement.com',
+            'HACKED_PRESS_LINK' : 'https://hacked.press'
             }
