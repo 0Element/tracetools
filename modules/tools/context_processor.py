c872554b46913771c47a6bd85391ccb1e751163e
Sergey Nelepa <sergey@zeroelement.io>
2017-05-31 00:06:13 +0300

    mini changes

--- modules/tools/context_processor.py
+++ modules/tools/context_processor.py
@@ -4,5 +4,6 @@ def toolset_proc(request):
     return {
             'GOOGLE_MAPS_KEY': settings.GOOGLE_MAPS_KEY,
             'MAIN_SITE_REDIRECT': 'https://zeroelement.com',
-            'HACKED_PRESS_LINK' : 'https://hacked.press'
+            'HACKED_PRESS_LINK' : 'https://hacked.press',
+            'ZERO_DOWNTIME_LINK': settings.ZERO_DOWNTIME_LINK
             }
